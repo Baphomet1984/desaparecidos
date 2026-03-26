e3959937329dc3b993d54ec43ce4f0466530c13a
system fail <120350681+Baphomet1984@users.noreply.github.com>
2026-03-26 17:55:38 -0600

    Update noticias.js

--- fuentes/noticias.js
+++ fuentes/noticias.js
@@ -1,1 +1,40 @@
+const axios = require("axios")
 
+async function obtenerNoticias(){
+
+  const url = "https://news.google.com/rss/search?q=desaparecido+Mexico"
+
+  const res = await axios.get(url)
+
+  const xml = res.data
+
+  const items = xml.split("<item>")
+
+  let casos = []
+
+  items.forEach(i=>{
+
+    const title = i.match(/<title>(.*?)<\/title>/)
+
+    if(title){
+
+      casos.push({
+        nombre:title[1],
+        edad:null,
+        sexo:null,
+        lugar:null,
+        fecha_desaparicion:null,
+        fecha_reporte:null,
+        fuente:"Noticias",
+        link:null
+      })
+
+    }
+
+  })
+
+  return casos
+
+}
+
+module.exports = obtenerNoticias
