6d19b6a2afd7ac8f8199e5c8b201cf55cdd70b9a
system fail <120350681+Baphomet1984@users.noreply.github.com>
2026-03-26 17:52:49 -0600

    Update rnpdno.js

--- fuentes/rnpdno.js
+++ fuentes/rnpdno.js
@@ -1,1 +1,38 @@
+const axios = require("axios")
 
+async function obtenerRNPDNO(){
+
+  const url = "https://versionpublicarnpdno.segob.gob.mx/Busqueda"
+
+  const res = await axios.get(url,{
+    headers:{
+      "User-Agent":"Mozilla/5.0"
+    }
+  })
+
+  let casos = []
+
+  if(res.data && res.data.resultados){
+
+    res.data.resultados.forEach(c=>{
+
+      casos.push({
+        nombre:c.nombre,
+        edad:c.edad,
+        sexo:c.sexo,
+        lugar:c.estado,
+        fecha_desaparicion:c.fechaDesaparicion,
+        fecha_reporte:c.fechaRegistro,
+        fuente:"RNPDNO",
+        link:"https://versionpublicarnpdno.segob.gob.mx/"
+      })
+
+    })
+
+  }
+
+  return casos
+
+}
+
+module.exports = obtenerRNPDNO
