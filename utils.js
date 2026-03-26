973764db715c07d94c516e1239233aa7e9ea1060
system fail <120350681+Baphomet1984@users.noreply.github.com>
2026-03-26 04:12:44 -0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,32 @@
+const fs = require("fs-extra")
 
+const DB_FILE = "./desaparecidos.json"
+
+async function cargarDB() {
+  if (!await fs.pathExists(DB_FILE)) {
+    await fs.writeJson(DB_FILE, [])
+  }
+  return await fs.readJson(DB_FILE)
+}
+
+async function guardarDB(data) {
+  await fs.writeJson(DB_FILE, data, { spaces: 2 })
+}
+
+function existeCaso(db, nombre, fecha) {
+  return db.find(
+    c => c.nombre === nombre && c.fechaDesaparicion === fecha
+  )
+}
+
+function generarID(db) {
+  const num = db.length + 1
+  return "CASO_" + String(num).padStart(4, "0")
+}
+
+module.exports = {
+  cargarDB,
+  guardarDB,
+  existeCaso,
+  generarID
+}
