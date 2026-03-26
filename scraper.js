e4fe032579741cc29ab1d24d80332beb68905402
system fail <120350681+Baphomet1984@users.noreply.github.com>
2026-03-26 04:14:15 -0600

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,1 +1,58 @@
+const obtenerAlertasAmber = require("./fuentes/amber")
+const {
+  cargarDB,
+  guardarDB,
+  existeCaso,
+  generarID
+} = require("./utils")
 
+async function ejecutarScraper() {
+
+  const db = await cargarDB()
+
+  const casos = await obtenerAlertasAmber()
+
+  for (const caso of casos) {
+
+    const existente = existeCaso(
+      db,
+      caso.nombre,
+      caso.fechaDesaparicion
+    )
+
+    if (!existente) {
+
+      const nuevo = {
+        id: generarID(db),
+        ...caso,
+
+        historial: [
+          {
+            fecha: caso.fechaDesaparicion,
+            estatus: caso.estatus,
+            nota: "Registro inicial desde scraping"
+          }
+        ],
+
+        creadoEn: new Date().toISOString()
+      }
+
+      db.push(nuevo)
+
+      console.log("Nuevo caso agregado:", caso.nombre)
+
+    } else {
+
+      existente.ultimaActualizacion = new Date().toISOString()
+
+    }
+
+  }
+
+  await guardarDB(db)
+
+  console.log("Scraping terminado")
+
+}
+
+ejecutarScraper()
